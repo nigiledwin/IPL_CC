8c015362924d5df8ac47fc8a7b315ba892e3d956
nigiledwin <nigilke9416@gmail.com>
2024-03-31 12:29:12 +0200

    initial commit

---
+++ cookiecuttertemplate/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='this is a cookiecutter template and can be used for all ML related projects',
+    author='nigil',
+    license='',
+)
