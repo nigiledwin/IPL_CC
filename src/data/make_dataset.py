09e40a1c1770f170c22ad0448a3b90f4ef0eece5
nigiledwin <nigilke9416@gmail.com>
2024-04-02 21:40:29 +0200

    added feature definitions

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,30 +1,76 @@
-# -*- coding: utf-8 -*-
-import click
-import logging
-from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+import pandas as pd
+import numpy as np
+import pathlib
 
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
-    """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
-
-
+# Importing dataframe
 if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
+    curr_dir = pathlib.Path(__file__)
+    home_dir = curr_dir.parent.parent.parent
 
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
+def load_data(data_path):
+    # Load your dataset from a given path
+    df = pd.read_csv(data_path)
+    return df
 
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
+data_path=home_dir.as_posix() + '/data/raw/all_season_details.csv'
+try:
+    df = load_data(data_path)
+except pd.errors.ParserError as e:
+    print("ParserError:", e)
 
-    main()
+# Columns to keep
+columns_to_keep = ['season', 'match_id', 'current_innings', 'innings_id', 'match_name', 'home_team', 'away_team', 'over', 'ball', 'runs', 'isBoundary', 'isWide', 'wicket_id', 'isNoball', 'batsman1_name', 'batsman_runs']
+df_filtered = df[columns_to_keep].copy()  # Ensure you're working on a copy
+
+# Adding additional columns
+df_filtered['total_runs'] = df.groupby(['match_id', 'innings_id'])['runs'].transform('cumsum').astype(int)
+df_filtered['Wkts'] = df_filtered.groupby(['match_id', 'innings_id'])['wicket_id'].transform(lambda x: x.notnull().cumsum()).astype(int)
+df_filtered['final_scores'] = df_filtered.groupby(['match_id', 'current_innings'])['runs'].transform('sum')
+df_filtered['1st_10overruns'] = df_filtered[df_filtered['over']<=10].groupby(['match_id', 'current_innings'])['runs'].transform('sum')
+df_filtered['1st_10overruns'] = df_filtered['1st_10overruns'].fillna(method='ffill')
+df_filtered['1st_10overwkts'] = df_filtered[df_filtered['over']<=10].groupby(['match_id', 'current_innings'])['wicket_id'].transform('count')
+df_filtered['1st_10overwkts'] = df_filtered['1st_10overwkts'].fillna(method='ffill')
+df_filtered['bowling_team'] = df_filtered.apply(lambda row: row['away_team'] if row['home_team'] == row['current_innings'] else row['home_team'], axis=1)
+df_filtered['Is_bat_home_team'] = df_filtered.apply(lambda row: 'Yes' if row['home_team'] == row['current_innings'] else 'No', axis=1)
+print(df_filtered.head())
+
+
+
+# Assuming df is your DataFrame containing the match data
+
+# Sort the DataFrame by season and match_id to ensure chronological order
+df_sorted = df_filtered.sort_values(by=['season', 'match_id'])
+
+# Group the DataFrame by the team
+grouped = df_sorted.groupby('current_innings')
+
+# Calculate the rolling mean of the final scores for the last 10 matches for each team
+rolling_mean = grouped['1st_10overruns'].rolling(window=10, min_periods=1).mean()
+
+# Reset the index to align the rolling mean values with the original DataFrame
+rolling_mean = rolling_mean.reset_index(level=0, drop=True)
+
+# Assign the rolling mean values to a new column in the original DataFrame
+df_filtered['last_10_matches_mean_10overscore'] = rolling_mean
+
+
+columns_to_select_ppruns=['current_innings','bowling_team','Is_bat_home_team','over', 'ball','total_runs','Wkts','1st_10overruns']
+df_pp_ml=df_filtered[columns_to_select_ppruns]
+df_pp_ml.rename(columns={'current_innings': 'batting_team'}, inplace=True)
+df_final=df_pp_ml[df_pp_ml['over']<=10]
+
+
+df_final.loc[df_final['batting_team'] == 'KXIP', 'batting_team'] = 'PBKS'
+df_final.loc[df_final['bowling_team'] == 'KXIP', 'bowling_team'] = 'PBKS'
+df_final.loc[df_final['batting_team'] == 'GL', 'batting_team'] = 'GT'
+df_final.loc[df_final['bowling_team'] == 'GL', 'bowling_team'] = 'GT'
+df_final.loc[df_final['batting_team'] == 'PWI', 'batting_team'] = 'RPS'
+df_final.loc[df_final['bowling_team'] == 'PWI', 'bowling_team'] = 'RPS'
+df_final = df_pp_ml[(df_pp_ml['1st_10overruns'] >= 15) & (df_pp_ml['1st_10overruns'] <= 150)]
+
+
+X=df_final.drop(columns=['1st_10overruns'])
+y=df_final['1st_10overruns']
+
+print(df_final.tail(5))
--- src/features/build_features.py
+++ src/features/build_features.py
@@ -2,16 +2,22 @@ import pathlib
 import pandas as pd
 import numpy as np
 
+from feature_definitions import load_data,df_10_predict,save_data
+
 from sklearn.model_selection import train_test_split
+
+
+# Importing dataframe
 
 if __name__ == '__main__':
     curr_dir = pathlib.Path(__file__)
     home_dir = curr_dir.parent.parent.parent
+    data_path=home_dir.as_posix() + '/data/raw/all_season_details.csv'
+    df=load_data(data_path)
+    df_10overruns=df_10_predict(df)
+    print(df_10overruns.head())
 
-data_path=home_dir.as_posix() + '/data/raw/all_season_details.csv'
-def load_data(data_path):
-    # Load your dataset from a given path
-    df = pd.read_csv(data_path)
-    return df
-
-df=load_data(data_path)
+    # Splitting the DataFrame into training (80%) and testing (20%) sets
+    train_ddf_10overruns, test_df_10overruns = train_test_split(df_10overruns, test_size=0.2, random_state=2024)
+    output_path = home_dir.as_posix() + '/data/processed'
+    save_data(train_ddf_10overruns,test_df_10overruns,output_path)
